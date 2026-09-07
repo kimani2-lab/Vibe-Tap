6a90db4508945fbd4760e7f6f6f220ca447af37f
kimani2-lab <kimania271@gmail.com>
2026-09-07 15:44:14 +0300

    Add 127.0.0.1 and localhost to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-)t%!u7$p0ji1urrjx=xt!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get('DEBUG') == 'False' else True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.onrender.com').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,.onrender.com').split(',')
 
 
 # Application definition
